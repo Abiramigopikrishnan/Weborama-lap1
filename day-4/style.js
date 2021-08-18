48f17996083b5d78f1db75e809baaa9129342ab9
Abirami Gopikrishnan <aamis1605@gmail.com>
2021-08-18 19:52:18 +0530

    “Weborama-lap1”

--- day-4/style.js
+++ day-4/style.js
@@ -1,32 +1,33 @@
-// let a=10
-// console.log(a)
+//displaying on console
+let a=10
+console.log(a)
 
-// //updation
-// let a=10
-// a=20 
-// console.log(a)
+//updation
+let a=10
+a=20 
+console.log(a)
 
-// let a=10
-// a=a+1 //error
-// console.log(a)
+let a=10
+a=a+1 //error
+console.log(a)
 
-// //constants
-// const a=10
-// a+=1
-// console.log(a)
+//constants
+const a=10
+a+=1
+console.log(a)
 
-// //Dynamic
-// let a=10
-// a="Hello"
-// console.log(a)
+//Dynamically typed
+let a=10
+a="Hello"
+console.log(a)
 
-// //function
-// func display(){
-//     console.log("Hai there")
-// }
-// display()
+//function
+func display(){
+    console.log("Hai there")
+}
+display()
 
-//arrowed function
+//arrow function
 let display=()=>{
     console.log("Hai there")
 }
